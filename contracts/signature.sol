ab619dcf3b7c7a688bd567983e2c1c8b4b2a9eeb
0xAdnanH <0xadnanh@gmail.com>
2023-06-13 20:39:32 +0300

    Update signature.sol

--- contracts/signature.sol
+++ contracts/signature.sol
@@ -2,15 +2,27 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
+/**
+ * @title CallBySignature
+ * @dev A contract that allows the owner to increment a call number by verifying a signature.
+ */
 contract CallBySignature {
     uint256 public count;
     uint256 public nonce;
     address public owner;
 
+    /**
+     * @dev Contract constructor
+     * @param _owner The address of the contract owner
+     */
     constructor(address _owner) {
         owner = _owner;
     }
 
+    /**
+     * @dev Increment the call number by verifying the provided signature.
+     * @param signature The signature to be verified
+     */
     function incrementCallNumber(bytes memory signature) public {
         bytes32 hash = ECDSA.toEthSignedMessageHash(
             abi.encodePacked(
